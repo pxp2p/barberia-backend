e134b0cfea207ca70517775a5484a9031be57f5d
Joaquin <joaquinpereirahernan@gmail.com>
2026-08-31 11:25:11 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// Ruta para registrar usuarios nuevos: POST a https://onrender.com
 router.post('/register', authController.register);
+
+// Ruta para el login real: POST a https://onrender.com
 router.post('/login', authController.login);
 
 module.exports = router;
