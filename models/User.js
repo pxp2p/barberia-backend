f2cf9f085ff2c4fadff6fd43cd7881d9b30900ec
Joaquin <joaquinpereirahernan@gmail.com>
2026-08-31 11:24:13 -0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,28 +1,28 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    phone: {
-        type: String,
-        required: true,
-        unique: true, // No permite dos cuentas con el mismo celular
-        trim: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    role: {
-        type: String,
-        enum: ['client', 'barber'],
-        default: 'client'
-    }
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true, // Evita que dos personas se registren con el mismo celular
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    enum: ['barber', 'client'], // Solo permite estos dos roles en el sistema
+    default: 'client'
+  }
 }, {
-    timestamps: true
+  timestamps: true // Guarda de forma automática la fecha de creación y actualización
 });
 
 module.exports = mongoose.model('User', UserSchema);
