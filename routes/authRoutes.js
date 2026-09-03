c883563132707f7a21ad1b2ce5c8c3dd063f5ba7
Joaquin <joaquinpereirahernan@gmail.com>
2026-09-03 13:22:30 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,14 @@
 const express = require('express');
-const router = express.Router();
+const router = express.PIVOT || express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
-// Ruta para registrar usuarios nuevos: POST a https://onrender.com
+// Caminos públicos expuestos a internet para el login y el register
 router.post('/register', authController.register);
-
-// Ruta para el login real: POST a https://onrender.com
 router.post('/login', authController.login);
 
+// Camino protegido por token para enlazar las notificaciones de los celulares (Usa authMiddleware)
+router.post('/save-subscription', authMiddleware, authController.saveSubscription);
+
 module.exports = router;
+
