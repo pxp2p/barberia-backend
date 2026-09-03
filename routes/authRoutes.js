814ecc62b8ee9a583064aa34f2cfedf29b324f1a
Joaquin <joaquinpereirahernan@gmail.com>
2026-09-03 13:28:07 -0300

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const router = express.PIVOT || express.Router();
+const router = express.Router(); // Corregido el tipado genérico
 const authController = require('../controllers/authController');
-const authMiddleware = require('../middleware/authMiddleware');
+const authMiddleware = require('../../middleware/authMiddleware'); // <-- ¡LE AGREGAMOS UN PUNTO MÁS PARA SALIR DE LA CARPETA ROUTES!
 
 // Caminos públicos expuestos a internet para el login y el register
 router.post('/register', authController.register);
 router.post('/login', authController.login);
 
-// Camino protegido por token para enlazar las notificaciones de los celulares (Usa authMiddleware)
+// Camino protegido por token para enlazar las notificaciones de los celulares
 router.post('/save-subscription', authMiddleware, authController.saveSubscription);
 
 module.exports = router;
-
